8f7f043b7a143ceaf0d3a39a87d8dc9406d4f7e1
grewwc <mamawwc@gmail.com>
2025-11-03 16:51:09 +0800

    update:oo

--- src/clipboard/image_content.rs
+++ src/clipboard/image_content.rs
@@ -32,16 +32,42 @@ fn open_by_content(path: &str) -> Result<image::DynamicImage, Box<dyn std::error
     let file = File::open(path)?;
     let mut reader = BufReader::new(file);
 
-    // Peek at first 8 bytes to detect format
-    let mut header = [0; 8];
+    // Peek at first 12 bytes to detect format (some formats need more bytes)
+    let mut header = [0; 12];
     reader.read_exact(&mut header)?;
 
     let format = if header.starts_with(&[0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]) {
+        // PNG magic bytes
         ImageFormat::Png
     } else if header.starts_with(&[0xFF, 0xD8, 0xFF]) {
+        // JPEG magic bytes
         ImageFormat::Jpeg
+    } else if header.starts_with(&[0x47, 0x49, 0x46, 0x38, 0x37, 0x61]) || 
+              header.starts_with(&[0x47, 0x49, 0x46, 0x38, 0x39, 0x61]) {
+        // GIF magic bytes (GIF87a or GIF89a)
+        ImageFormat::Gif
+    } else if header.starts_with(&[0x52, 0x49, 0x46, 0x46]) && 
+              header[8..12] == [0x57, 0x45, 0x42, 0x50] {
+        // WebP magic bytes (RIFF....WEBP)
+        ImageFormat::WebP
+    } else if header.starts_with(&[0x42, 0x4D]) {
+        // BMP magic bytes
+        ImageFormat::Bmp
+    } else if header.starts_with(&[0x49, 0x49, 0x2A, 0x00]) || 
+              header.starts_with(&[0x4D, 0x4D, 0x00, 0x2A]) {
+        // TIFF magic bytes (little-endian or big-endian)
+        ImageFormat::Tiff
+    } else if header.starts_with(&[0x00, 0x00, 0x01, 0x00]) {
+        // ICO magic bytes
+        ImageFormat::Ico
+    } else if header[0..4] == [0x71, 0x6F, 0x69, 0x66] {
+        // QOI magic bytes "qoif"
+        ImageFormat::Qoi
     } else {
-        return Err("Unsupported image format".into());
+        return Err(format!(
+            "Unsupported image format. Supported formats: PNG, JPEG, GIF, WebP, BMP, TIFF, ICO, QOI. Header bytes: {:02X?}", 
+            &header[..8]
+        ).into());
     };
 
     // Rewind by creating a new reader with header + rest
@@ -79,3 +105,4 @@ pub fn copy_from_file(fname: &str) -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 }
+
